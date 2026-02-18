7d1ff21802cbecffc0a50c930e14f287ec12464b
ODS <idsorlyk@gmail.com>
2026-02-18 22:34:33 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
 import {
-    initMongoDB
-} from './db/initMongoDB.js';
-import authRouter from './routes/auth.js';
+    initMongoDBConnection
+} from './db/initMongoDBConnection.js';
+import {
+    setupServer
+} from './app.js';
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use('/api/auth', authRouter);
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Podorozhnyky API is running 🚀 '
-    });
-});
-
-const startServer = async () => {
-    try {
-        await initMongoDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Failed to start server 🌵 :', error);
-    }
+const bootstrap = async () => {
+    await initMongoDBConnection();
+    setupServer();
 };
 
-startServer();
+bootstrap();
