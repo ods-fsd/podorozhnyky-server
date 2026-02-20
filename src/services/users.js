8169951aa27d2d448b679ac75158985fcde3f46c
BilykYarosalvAleksandrovich <bilyk.yaroslav.aleksandrovich@gmail.com>
2026-02-20 10:19:58 +0200

    pagination-client

--- src/services/users.js
+++ src/services/users.js
@@ -1,51 +1,65 @@
-import { UsersCollection } from '../db/models/user.js';
-import { StoriesCollection } from '../db/models/story.js';
+import { UsersCollection } from "../db/models/user.js";
+import { StoriesCollection } from "../db/models/story.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
 
-import createHttpError from 'http-errors';
+import createHttpError from "http-errors";
 
 export const getUserCurrentService = async (userId, { page, perPage }) => {
-  
   const skip = (page - 1) * perPage;
 
-  
   const user = await UsersCollection.findOne({ _id: userId }).select(
-    'favorites name email avatarUrl description',
+    "favorites name email avatarUrl description"
   );
 
-  
   if (!user) {
     return { user: null, totalFavoritesCount: 0 };
   }
 
-  
   const totalFavoritesCount = user.favorites.length;
 
-  
   const paginatedFavoriteIds = user.favorites.slice(skip, skip + perPage);
 
-  
   const paginatedFavorites = await StoriesCollection.find({
     _id: { $in: paginatedFavoriteIds },
   }).populate([
     {
-      path: 'ownerId',
-      select: 'name email avatarUrl description',
+      path: "ownerId",
+      select: "name email avatarUrl description",
     },
     {
-      path: 'category',
-      select: '_id name',
+      path: "category",
+      select: "_id name",
     },
   ]);
 
-   
   const userObject = user.toObject();
-  delete userObject.favorites; 
+  delete userObject.favorites;
 
   const finalUser = {
     ...userObject,
     favorites: paginatedFavorites,
   };
 
-  
   return { user: finalUser, totalFavoritesCount };
+};
+
+export const getAllUsersService = async ({ page, perPage }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const usersQuery = UsersCollection.find().select(
+    "name avatarUrl description"
+  );
+
+  const [usersCount, users] = await Promise.all([
+    UsersCollection.countDocuments(),
+    usersQuery.skip(skip).limit(limit).exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(usersCount, perPage, page);
+
+  return {
+    data: users,
+    ...paginationData,
+  };
 };
