4ae99f2a5bcffbf18b87cf7dae602106db29a83b
ODS <idsorlyk@gmail.com>
2026-03-03 20:22:04 +0100

    feat: Add email sending utility using nodemailer.

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,7 +3,6 @@ import nodemailer from 'nodemailer';
 const config = {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_SECURE === 'true',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
