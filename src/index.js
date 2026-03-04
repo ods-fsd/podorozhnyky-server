53f4f7cd8a656f550831b8947637eeb82ca944f7
unknown <mykola.kruhlyk.pl@gmail.com>
2026-03-04 19:11:30 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {
     setupServer
 } from './server.js';
-// Використовуємо твою назву файлу з 'DB'
+
 import {
     initMongoDBConnection
 } from './db/initMongoDBConnection.js';
@@ -13,23 +13,20 @@ import {
     UPLOAD_DIR
 } from './constants/index.js';
 
-/**
- * Головна функція запуску (bootstrap).
- * Вона готує середовище перед тим, як сервер почне приймати запити.
- */
+
 const bootstrap = async () => {
-    // 1. Чекаємо на успішне підключення до бази даних
+   
     await initMongoDBConnection();
 
-    // 2. Створюємо папку для тимчасових файлів (temp), якщо її немає
+    
     await createDirIfNotExists(TEMP_UPLOAD_DIR);
 
-    // 3. Створюємо папку для постійних файлів (uploads), якщо її немає
+    
     await createDirIfNotExists(UPLOAD_DIR);
 
-    // 4. Запускаємо основний Express-сервер із файлу server.js
+   
     setupServer();
 };
 
-// Запускаємо весь процес
+
 void bootstrap();
